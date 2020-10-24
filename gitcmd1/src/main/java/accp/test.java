24b36230a2c6ba10517f53c4ae7cb4300bd28fb3
zhangcuoji92 <342055372@qq.com>
2020-10-24 16:12:10 +0800

    第二次提交

--- gitcmd1/src/main/java/accp/test.java
+++ gitcmd1/src/main/java/accp/test.java
@@ -4,5 +4,7 @@ public class test {
     public static void main(String[] args) {
         System.out.println("idea第一次测试");
         System.out.println("66666");
+
+        System.out.println("第二次提交" );
     }
 }
